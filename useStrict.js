6171287e676eed980d8005a3a0d5b0030438b387
maheshsthr <msx31.mahesh@gmail.com>
2026-09-14 16:39:49 +0530

    use strict

---
+++ useStrict.js
@@ -0,0 +1,11 @@
+"use strict";
+x = 10
+console.log(x)
+
+//use strict will make sure that variable used is a declared one before use 
+
+{
+    y=20
+    console.log(y)
+}
+//it will not show error because the use strict is out of this block not this block scoped
