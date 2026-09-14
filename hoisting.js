590053cfd2bd0f37ffd13b7b26ea080d88e35e5c
maheshsthr <msx31.mahesh@gmail.com>
2026-09-14 16:34:26 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,11 @@
+//hoisting is ability to use var before initializing it 
+
+console.log(x)
+
+var x=10
+
+//let and const declarations after access of variable will have error
+
+console.log(y)
+
+let y = 10
