ad62194aea759d4a64842834fc2117219034b973
maheshsthr <msx31.mahesh@gmail.com>
2026-09-14 14:31:11 +0530

    object dot and square bracket notation

---
+++ objects/dotNotation.js
@@ -0,0 +1,11 @@
+const student = {
+    name : "Mahesh",
+    course : "BS(CA)",
+    college: "LJK",
+    track: "Software Developer"
+}
+
+console.log(student.name + "is pursuing "+student.course + " and is on track of "+ student.track)
+console.log(student['name'] + "is pursuing "+ student['course'] + " and is on track of "+ student['track'])
+
+// dot notation is preferred for easyness and simplicity
---
+++ objects/object.js
@@ -0,0 +1,14 @@
+const student = {
+    name : "Mahesh",
+    course : "BCA",
+    age : 19,
+    college : "LJK",
+}
+
+console.log(student)
+
+student.name = "Mahesh Suthar"
+console.log(student)
+
+student['course'] = "BS Computer Applications"
+console.log(student)
