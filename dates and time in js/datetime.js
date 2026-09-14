f62a1f2c9a8d2fe6387634947c1d4500a2949e1e
maheshsthr <msx31.mahesh@gmail.com>
2026-09-14 14:00:13 +0530

    js dates and times

---
+++ dates and time in js/datetime.js
@@ -0,0 +1,14 @@
+const myDate = new Date("2025-05-13")
+
+console.log(myDate.toString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toDateString())
+
+const nowDate = new Date();
+console.log(nowDate)
+
+console.log(nowDate.getMonth()+1)
+console.log(nowDate.getFullYear())
+console.log(nowDate.getDate())
+
