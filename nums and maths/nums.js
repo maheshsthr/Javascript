bc7c361003d2775bda72c1caf30569b12beab0c9
maheshsthr <msx31.mahesh@gmail.com>
2026-09-14 13:35:42 +0530

    JS numbers

---
+++ nums and maths/nums.js
@@ -0,0 +1,17 @@
+const score = 400 
+const balance = new Number(500)
+
+console.log(balance)
+console.log(score)
+
+console.log(balance.toString())
+
+console.log(balance.toFixed(2))
+
+const otherNum = 500.600
+console.log(otherNum.toPrecision(4))
+
+const hundreds = 23187322
+console.log(hundreds.toLocaleString('en-IN'))
+10000022
+
