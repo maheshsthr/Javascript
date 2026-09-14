f66b38b5ee3071b98d6c957d02ac0c751f3e3320
maheshsthr <msx31.mahesh@gmail.com>
2026-09-14 11:37:46 +0530

    Strings Complete

--- strings/stringSearch.js
+++ strings/stringSearch.js
@@ -4,3 +4,12 @@ let str = "Hello My name is Mahesh , and my full name is Mahesh Suthar"
 console.log("Index of Mahesh is ",str.indexOf("Mahesh"))
 
 console.log("Last index of Mahesh is ",str.lastIndexOf("Mahesh",))
+
+//search method
+str = "mahesh suthar"
+console.log(str.search("suthar"))
+
+//match 
+str = "he is a good person"
+console.table(str.match("good"))
+console.log(str.match("Mahesh"))
