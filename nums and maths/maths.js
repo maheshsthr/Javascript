01e474f426f92d18e40be78d4a2eecb3a18158a6
maheshsthr <msx31.mahesh@gmail.com>
2026-09-14 13:46:15 +0530

    js maths

---
+++ nums and maths/maths.js
@@ -0,0 +1,15 @@
+console.log(Math) 
+console.log(Math.abs(-599))
+//abs function makes negatives to positives but didnt change positive to negatives
+
+console.log(Math.round(5.76)) //6
+console.log(Math.round(5.23)) //5
+console.log(Math.round(5.5))  //6
+
+//ceil and floor
+console.log(Math.ceil(5.6))  //6
+console.log(Math.floor(5.6)) //5
+
+//random
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
