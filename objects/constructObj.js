14b9cfd6ebb411239bf8e167da1b44fda0f74c11
maheshsthr <msx31.mahesh@gmail.com>
2026-09-15 12:11:30 +0530

    object constructor and method call

--- objects/constructObj.js
+++ objects/constructObj.js
@@ -2,6 +2,9 @@ function car(name,year,price){
     this.name =name
     this.year = year
     this.price = price
+    this.start = function(){
+        console.log(`${this.name} Started`)
+    }
 }
 
 const car1 = new car('KIA','SONET GTX',1500000)
@@ -12,3 +15,7 @@ console.log(car2)
 
 const car3 = new car('Toyota','Legender Plus',6500000)
 console.log(car3)
+
+car1.start()
+car2.start()
+car3.start()
