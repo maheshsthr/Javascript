c62e19d9df9937197039e191bcb26de9fde1165d
maheshsthr <msx31.mahesh@gmail.com>
2026-09-15 12:06:15 +0530

    use constructor function for object

---
+++ objects/constructObj.js
@@ -0,0 +1,14 @@
+function car(name,year,price){
+    this.name =name
+    this.year = year
+    this.price = price
+}
+
+const car1 = new car('KIA','SONET GTX',1500000)
+console.log(car1)
+
+const car2 = new car('Mahindra','XUV3XO AX7L',1600000)
+console.log(car2)
+
+const car3 = new car('Toyota','Legender Plus',6500000)
+console.log(car3)
