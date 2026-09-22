39ac8d82e47ab45be98d98dfa09ced819d64ec2c
maheshsthr <msx31.mahesh@gmail.com>
2026-09-22 17:05:35 +0530

    mini github api project

---
+++ mini projects/github.js
@@ -0,0 +1,26 @@
+const getUser = async (username)=>{
+    try {  
+        const response = await fetch(`https://api.github.com/users/${username}`)
+        const userProfile = await response.json()
+        if(!response.ok){
+            throw new Error("Failed to fetch User")
+        }
+        return userProfile;
+    }
+    catch(error){
+        console.log(error.message)
+    }
+}
+
+const uname = "maheshsthr"
+const user=getUser(uname).then((user)=>{
+    console.log(`Github Profile`)
+    console.log(`Name : ${user.name}`)
+    console.log(`Location : ${user.location ?? "Not Provided"}`)
+    console.log(`Bio : ${user.bio ?? "Not Provided"}`)
+    console.log(`Public Repositories : ${user.public_repos}`)
+    console.log(`Followers : ${user.followers} Followings : ${user.following}`)
+    console.log(`Account Creation Date : ${new Date(user.created_at).toLocaleDateString()}`)
+})
+
+//name , location , bio , public repos , followers, following , created at
